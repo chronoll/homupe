93aea7e437a77f795e81ddb319e68b6324e291a0
chronoll <kurotaka.wa@gmail.com>
2025-08-16 15:00:34 +0900

    modify error

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,8 @@
+
 module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-  },
-  transform: {
-    '^.+\.(ts|tsx|js|jsx)?$': 'babel-jest',
   },
 };
