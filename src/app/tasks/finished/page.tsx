223c8797342d3bca754cfe5b1b8649eb82032872
chronoll <kurotaka.wa@gmail.com>
2025-08-16 14:26:13 +0900

    タスク9.1: FinishedTasksPageコンポーネントを実装

---
+++ src/app/tasks/finished/page.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const FinishedTasksPage = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">完了したタスク</h1>
+      <p>ここに完了したタスクの一覧が表示されます。</p>
+    </div>
+  );
+};
+
+export default FinishedTasksPage;
